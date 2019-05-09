73012372f3684424440a7e082e300509b6ca47ec
Trish Ogen <patriciaogen@gmail.com>
2019-05-09 01:40:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,9 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-    "Fizzbuzz"
+    puts "Fizzbuzz"
   elsif int % 3 == 0
-    "Fizz"
+    puts "Fizz"
   elsif int % 5 == 0
-    "Buzz"
+    puts "Buzz"
 end
